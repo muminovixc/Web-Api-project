36fe241929bdef1719f3f95dd40bf80e76568e5e
muminovixc <edin.muminovic@student.pmf.unsa.ba>
2025-06-15 17:00:23 +0200

    Refactor build.gradle.kts: switch from alias to id for Android and Kotlin plugins to improve clarity and maintainability.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    kotlin("kapt")
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {

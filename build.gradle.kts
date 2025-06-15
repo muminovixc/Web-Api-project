0d910b7644ba9cdeeb8aa179ebbf8766f332792b
muminovixc <edin.muminovic@student.pmf.unsa.ba>
2025-06-15 18:00:05 +0200

    Refactor build.gradle.kts: remove redundant repository declarations and streamline configuration for improved clarity.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,4 @@
 buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
@@ -12,13 +8,6 @@ buildscript {
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
 }
 
 tasks.register("clean", Delete::class) {
